a6f7f5e7f49988575dca7214367498ad8efb87fe
Aleksander Gorecki <aleksandergorecki80@gmail.com>
2019-12-03 21:04:08 +0000

    Create a ship

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 
+// Creating a grid and adding id to each field
 function gridPrint(id) {
+    const game = document.getElementById('game');
     var div = document.createElement("DIV");
-    document.body.appendChild(div);
+    game.appendChild(div);
     div.classList.add('field');
     div.id = id;
 }
@@ -9,3 +11,31 @@ function gridPrint(id) {
 for(let i=0; i<7; i++){
     gridPrint(i);
 }
+
+// Setting up a first field of a ship
+const shipStart = Math.floor(Math.random() * 5);
+const ship = [shipStart];
+
+//bilding a ship
+function buildingShip(shipStart){
+    for(let i=1; i<3; i++){
+        ship.push(shipStart+i);
+    }
+    
+    ship.forEach(id => {
+        document.getElementById(id).classList.add('ship');
+    });
+}
+buildingShip(shipStart)
+
+const game = document.getElementsByClassName('field');
+const gameArray = Array.from(game)
+
+gameArray.forEach(field => {
+    field.addEventListener('click', () => {
+        console.log(field);
+    });    
+});
+
+console.log(ship);
+
