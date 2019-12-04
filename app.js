f8e559eb64a69a57106fe748e44ad9f62f0b1046
Aleksander Gorecki <aleksandergorecki80@gmail.com>
2019-12-04 21:53:15 +0000

    Add a control if field was already hit and a reset functinality

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-
 class Ship {
-  constructor(startShip) {
-    this.ship = [startShip];
-    this.poprawto = [0,1,2,3,4,5,6];
+  constructor() {
+    this.ship = [];
+    this.shipAtTheBegining = [];
   }
   buildingShip() {
-    for (let i = 1; i < 3; i++) {
+    // Setting up a first field of a ship
+    const shipStart = Math.floor(Math.random() * 5);
+    for (let i = 0; i < 3; i++) {
       this.ship.push(shipStart + i);
-    //   this.markTheShip();
+      this.shipAtTheBegining.push(shipStart + i);
+      // this.markTheShip();
     }
   }
   hitTheSheep(number) {
@@ -19,52 +21,58 @@ class Ship {
     this.ship.forEach(id => {
       document.getElementById(id).classList.add("ship");
     });
+    
+    
   }
-  burnTheShip(id){
+  burnTheShip(id) {
     document.getElementById(id).classList.add("burn");
-    if(this.ship.length === 0 ){
-        alert('Koniec');
-        
-        this.reset();
+    if (this.ship.length === 0) {
+      alert("Koniec");
+
+      this.reset();
     }
   }
-  reset(){
-    this.poprawto.forEach(id => {
-        document.getElementById(id).classList.remove("burn");
-      });
-      this.buildingShip();
+  reset() {
+    this.shipAtTheBegining.forEach(id => {
+      document.getElementById(id).classList.remove("burn");
+    });
+    this.ship = [];
+    this.shipAtTheBegining = [];
+    this.buildingShip();
   }
-  checkIfHit(id){
+  checkIfHit(id) {
     const intNr = parseInt(id);
-      if(this.ship.includes(intNr)){
-          this.hitTheSheep(intNr)
+    if (this.shipAtTheBegining.includes(intNr) && !this.ship.includes(intNr)) {
+      alert("You've already hit here!");
+      console.log(this.shipAtTheBegining);
+    } else {
+      if (this.ship.includes(intNr)) {
+        this.hitTheSheep(intNr);
       } else {
-        alert('Pudlo');
+        alert("Pudlo");
       }
-
+    }
   }
 }
 
-
 // Printing the grid
 function gridPrint(id) {
-    const game = document.getElementById("game");
-    var div = document.createElement("DIV");
-    game.appendChild(div);
-    div.classList.add("field");
-    div.id = id;
-  }
-  
-  for (let i = 0; i < 7; i++) {
-    gridPrint(i);
-  }
-  
-  // Setting up a first field of a ship
-  const shipStart = Math.floor(Math.random() * 5);
-  
+  const game = document.getElementById("game");
+  var div = document.createElement("DIV");
+  game.appendChild(div);
+  div.classList.add("field");
+  div.id = id;
+}
 
-const ship = new Ship(shipStart);
-ship.buildingShip(shipStart);
+for (let i = 0; i < 7; i++) {
+  gridPrint(i);
+}
+
+
+
+
+const ship = new Ship();
+ship.buildingShip();
 
 const game = document.getElementsByClassName("field");
 const gameArray = Array.from(game);
@@ -73,6 +81,5 @@ for (let i = 0; i < gameArray.length; i++) {
   const shotField = document.getElementById(i);
   shotField.addEventListener("click", () => {
     ship.checkIfHit(shotField.id);
-    console.log(ship.ship);
   });
 }
